f42e476ee71c8772c2d461eb49d389a6497ff49a
dwatz10 <dwatz10@gmail.com>
2016-09-19 14:32:23 -0500

    Submit

--- main.py
+++ main.py
@@ -1,25 +1,76 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
+import os
+import jinja2
+from google.appengine.ext import db
 
-class MainHandler(webapp2.RequestHandler):
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class BPost(db.Model):
+    title = db.StringProperty(required=True)
+    body = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+
+class Main(Handler):
     def get(self):
-        self.response.write('Hello world!')
+        self.redirect("/blog")
+
+class Blog(Handler):
+    def render_blog(self, title="", body=""):
+        bposts = db.GqlQuery("SELECT * FROM BPost ORDER BY created DESC LIMIT 5")
+        self.render("blog.html", title=title, body=body, bposts=bposts)
+
+    def get(self):
+        self.render_blog()
+
+    def post(self):
+        self.redirect("/newpost")
+
+class NewPost(Handler):
+    def render_new(self, title="", body="", error=""):
+        self.render("newpost.html", title=title, body=body, error=error)
+
+    def get(self):
+        self.render_new()
+
+    def post(self):
+        title = self.request.get("title")
+        body = self.request.get("body")
+
+        if title and body:
+            b = BPost(title=title, body=body)
+            b.put()
+
+            self.redirect("/blog/"+str(b.key().id()))
+        else:
+            error = "Submissions must include a title and body."
+            self.render_new(title, body, error)
+
+class ViewPostHandler(Handler):
+    def get(self, id):
+        bpost = BPost.get_by_id(int(id))
+        title = ""
+        body = ""
+
+        if bpost:
+            self.render("post.html", title=title, body=body, bpost=bpost)
+        else:
+            self.response.write("This post doesn't exist")
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', Main),
+    ('/blog', Blog),
+    ('/newpost', NewPost),
+    webapp2.Route('/blog/<id:\d+>', ViewPostHandler)
 ], debug=True)
